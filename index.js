a366a4e761d429b4bd283eca1365108a62c789e4
suraj-sigma-tech-north <130464388+suraj-sigma-tech-north@users.noreply.github.com>
2023-04-27 09:47:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function(request, response) {
+const hostname = 'localhost';
+const port = 8080;
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
 
-var port = 8080;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+server.listen(port, () => {
+  console.log('Server running');
+});
